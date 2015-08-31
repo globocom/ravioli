f0597985a7d75538f8aa8281451583d954a26b37
Almir Filho <almirflorenciofilho@gmail.com>
2015-08-31 19:53:25 -0300

    Adds textarea input component

---
+++ src/scripts/components/textarea.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+var GnocchiTextarea = React.createClass({
+  render: function(){
+    return (
+      <div class="gnocchi">
+        <div class="field">placeholder textarea</div>
+        <textarea></textarea>
+      </div>
+    );
+  }
+});
