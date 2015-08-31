7594227b4d554c429ee408455e8883ca72dda65e
Almir Filho <almirflorenciofilho@gmail.com>
2015-08-31 19:54:35 -0300

    Adds docs app

---
+++ docs/app.js
@@ -0,0 +1,7 @@
+React.render(
+  React.createElement('div', null,
+    React.createElement(GnocchiText, null),
+    React.createElement(GnocchiTextarea, null)
+  ),
+  document.getElementsByTagName('main')[0]
+);
