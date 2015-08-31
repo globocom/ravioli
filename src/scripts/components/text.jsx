7b9f5436b4b33e46767baa4fec84ed80e7066624
Almir Filho <almirflorenciofilho@gmail.com>
2015-08-31 19:53:08 -0300

    Adds text input component

---
+++ src/scripts/components/text.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+var GnocchiText = React.createClass({
+  render: function(){
+    return (
+      <div class="gnocchi">
+        <div class="field">placeholder text</div>
+        <input type="text" />
+      </div>
+    );
+  }
+});
