4835b57d81d53b469568ee9d91fbee3113fc05c1
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-01 18:06:08 -0300

    Adds focus and blur event handlers for text component

--- src/scripts/components/text.jsx
+++ src/scripts/components/text.jsx
@@ -2,11 +2,22 @@
 
 var GnocchiText = React.createClass({
 
+  focus: function(event){
+    this.getDOMNode().classList.add('gnocchi-focus');
+  },
+
+  blur: function(event){
+    this.getDOMNode().classList.remove('gnocchi-focus');
+  },
+
   render: function(){
     return (
       <div className='gnocchi-text'>
-        <input type='text'
-          placeholder={this.props.placeholder || 'Type something'} />
+        <input className='gnocchi-text-input'
+          type='text'
+          placeholder={this.props.placeholder || 'Type something'}
+          onFocus={this.focus}
+          onBlur={this.blur} />
       </div>
     );
   }
