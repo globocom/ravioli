b7df2ca6617d65e3b52d0a71289ba0342560708a
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-01 15:36:33 -0300

    Simplifies text component

--- src/scripts/components/text.jsx
+++ src/scripts/components/text.jsx
@@ -1,11 +1,11 @@
 'use strict';
 
 var GnocchiText = React.createClass({
+
   render: function(){
     return (
-      <div class="gnocchi">
-        <div class="field">placeholder text</div>
-        <input type="text" />
+      <div className='gnocchi-text'>
+        <input type='text' placeholder='dasdasdasdas' />
       </div>
     );
   }
