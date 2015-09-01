9a2ff15af9764a9d68a50e44ddd2ecfa6d3f5e8d
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-01 18:57:17 -0300

    Adds getDefaultProps to text component

--- src/scripts/components/text.jsx
+++ src/scripts/components/text.jsx
@@ -1,15 +1,20 @@
 'use strict';
 
 var GnocchiText = React.createClass({
-
   mixins: [FocusMixin],
+
+  getDefaultProps: function(){
+    return {
+      placeholder: 'Type something'
+    };
+  },
 
   render: function(){
     return (
       <div className='gnocchi-text'>
         <input className='gnocchi-text-input'
           type='text'
-          placeholder={this.props.placeholder || 'Type something'}
+          placeholder={this.props.placeholder}
           onFocus={this.focus}
           onBlur={this.blur} />
       </div>
