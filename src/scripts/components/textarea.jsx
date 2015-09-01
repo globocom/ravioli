301daaa6cbb7678042ac1b8b99a364e501aefb45
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-01 18:06:22 -0300

    Adds focus and blur event handlers for textarea component

--- src/scripts/components/textarea.jsx
+++ src/scripts/components/textarea.jsx
@@ -1,12 +1,23 @@
 'use strict';
 
 var GnocchiTextarea = React.createClass({
+
+  focus: function(event){
+    this.getDOMNode().classList.add('gnocchi-focus');
+  },
+
+  blur: function(event){
+    this.getDOMNode().classList.remove('gnocchi-focus');
+  },
+
   render: function(){
     return (
-      <div className="gnocchi-textarea">
-        <textarea
+      <div className='gnocchi-textarea'>
+        <textarea className='gnocchi-textarea-input'
           rows={this.props.lines || 4}
-          placeholder={this.props.placeholder || 'Type something'}>
+          placeholder={this.props.placeholder || 'Type something'}
+          onFocus={this.focus}
+          onBlur={this.blur}>
         </textarea>
       </div>
     );
