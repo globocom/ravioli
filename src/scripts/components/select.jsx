922b249aecc6ce1fde7a639ced3c51af31d43486
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-04 11:55:43 -0300

    Adds select component template

--- src/scripts/components/select.jsx
+++ src/scripts/components/select.jsx
@@ -3,14 +3,26 @@
 
 var GnocchiSelect = React.createClass({
   render: function(){
-    var options = this.props.options.map(function(option){
-      return <div>{option.value || option} - {option.label || option}</div>;
-    });
-
     return (
-      <div>
-        {options}
+      <div className='gnocchi-select gnocchi--is-open'>
+        <div className='gnocchi-text'>
+          <div className='gnocchi-select-display'>placeholder / value</div>
+          <div className='gnocchi-select-button'>
+            <i className='gnocchi-icon gnocchi-icon-arrow-down'></i>
+          </div>
+        </div>
+        <ul className='gnocchi-select-options'>
+          {this.props.options.map(this.renderOption)}
+        </ul>
       </div>
+    );
+  },
+
+  renderOption: function(option){
+    return (
+      <li className='gnocchi-select-option' data-value={option.value || option}>
+        {option.label || option}
+      </li>
     );
   }
 });
