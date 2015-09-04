4805ec8f158ea4eca0895b8a9e5d2c3219027797
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-04 15:41:55 -0300

    Adds open/close behaviors to select component

--- src/scripts/components/select.jsx
+++ src/scripts/components/select.jsx
@@ -1,14 +1,44 @@
-/* global React */
+/* global React, console */
 'use strict';
 
 var GnocchiSelect = React.createClass({
+  getInitialState: function(){
+    return {
+      open: false
+    };
+  },
+
+  keydown: function(event){
+    if(this.state.open){
+      if(event.which === 27){ // esc
+        this.toggle();
+      } else{
+        // select
+      }
+    } else if(event.which === 40 || event.which === 13 || event.which === 32){
+      this.toggle();
+    }
+
+    event.preventDefault();
+  },
+
+  toggle: function(){
+    this.setState({open: !this.state.open});
+  },
+
   render: function(){
+    var className = 'gnocchi-select';
+    if(this.state.open) className += ' gnocchi--is-open';
+
+    var iconClassName = 'gnocchi-icon gnocchi-icon-arrow-';
+    iconClassName += this.state.open ? 'up' : 'down';
+
     return (
-      <div className='gnocchi-select gnocchi--is-open'>
-        <div className='gnocchi-text'>
+      <div className={className} tabIndex='0' onKeyDown={this.keydown}>
+        <div className='gnocchi-text' onClick={this.toggle}>
           <div className='gnocchi-select-display'>placeholder / value</div>
           <div className='gnocchi-select-button'>
-            <i className='gnocchi-icon gnocchi-icon-arrow-down'></i>
+            <i className={iconClassName}></i>
           </div>
         </div>
         <ul className='gnocchi-select-options'>
