1e084553221825c333b8fa6ff31956c5def07d98
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-04 00:25:02 -0300

    Adds style modifiers for button component

--- docs/assets/app.jsx
+++ docs/assets/app.jsx
@@ -16,9 +16,15 @@ React.render(
 React.render(
   <div>
     <GnocchiButton>Default</GnocchiButton>
+    <GnocchiButton>
+      With icon
+      <i className='gnocchi-icon gnocchi-icon-arrow-right'></i>
+    </GnocchiButton>
     <GnocchiButton classes={['action']}>Action</GnocchiButton>
     <GnocchiButton classes={['highlight']}>Highlight</GnocchiButton>
     <GnocchiButton classes={['lowlight']}>Lowlight</GnocchiButton>
+    <GnocchiButton classes={['big']}>Big</GnocchiButton>
+    <GnocchiButton classes={['block']}>Block</GnocchiButton>
   </div>,
   document.querySelectorAll('#section-button')[0]
 );
