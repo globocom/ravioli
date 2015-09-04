a7b0233f5768d2a5212bebfd751ac0533c85eee8
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-04 18:18:03 -0300

    Adds focus control by keyboard in select component options

--- src/scripts/components/select.jsx
+++ src/scripts/components/select.jsx
@@ -5,6 +5,7 @@ var GnocchiSelect = React.createClass({
   getInitialState: function(){
     return {
       open: false,
+      focusedOption: null,
       selectedOption: null
     };
   },
@@ -31,9 +32,23 @@ var GnocchiSelect = React.createClass({
     this.setState({open: !this.state.open});
   },
 
-  up: function(){},
+  up: function(){
+    if(this.state.focusedOption > 0){
+      this.setState({focusedOption: this.state.focusedOption - 1});
+    }
+  },
 
-  down: function(){},
+  down: function(){
+    var newFocusedOption = this.state.focusedOption;
+
+    if(this.state.focusedOption === null){
+      newFocusedOption = 0;
+    } else if(this.state.focusedOption < this.props.options.length - 1){
+      newFocusedOption++;
+    }
+
+    this.setState({focusedOption: newFocusedOption});
+  },
 
   select: function(){},
 
@@ -47,7 +62,10 @@ var GnocchiSelect = React.createClass({
     return (
       <div className={className} tabIndex='0' onKeyDown={this.keydown}>
         <div className='gnocchi-text' onClick={this.toggle}>
-          <div className='gnocchi-select-display'>{this.state.selectedOption}</div>
+          <div className='gnocchi-select-display'>
+            - selected: {this.state.selectedOption}
+            - focused: {this.state.focusedOption}
+          </div>
           <div className='gnocchi-select-button'>
             <i className={iconClassName}></i>
           </div>
@@ -59,9 +77,12 @@ var GnocchiSelect = React.createClass({
     );
   },
 
-  renderOption: function(option){
+  renderOption: function(option, i){
+    var className = 'gnocchi-select-option';
+    if(i === this.state.focusedOption) className += ' gnocchi--is-focused';
+
     return (
-      <li className='gnocchi-select-option' data-value={option.value || option}>
+      <li className={className} data-value={option.value || option}>
         {option.label || option}
       </li>
     );
