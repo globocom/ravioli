dfaf65e400b732d38d67b8750ed23f03b4ccf9ae
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-09 11:08:52 -0300

    Adds unit tests for number component

--- test/components/number-test.js
+++ test/components/number-test.js
@@ -32,7 +32,70 @@ describe('GnocchiNumber', function(){
   });
 
   describe('Unit', function(){
+    before(function(){
+      this.component = utils.render(GnocchiNumber);
+    });
 
+    after(function(){
+      this.component = null;
+    });
+
+    context('#setValue()', function(){
+      it('should set zero when pass zero', function(){
+        this.component.setValue(0);
+        expect(this.component.props.value).to.equal(0);
+      });
+
+      it('should set number when pass number', function(){
+        this.component.setValue(20);
+        expect(this.component.props.value).to.equal(20);
+      });
+
+      it('should set number when pass string number', function(){
+        this.component.setValue('20');
+        expect(this.component.props.value).to.equal(20);
+      });
+
+      it('should set empty string when pass empty string', function(){
+        this.component.setValue('');
+        expect(this.component.props.value).to.equal('');
+      });
+
+      it('should set empty string when pass not a number string', function(){
+        this.component.setValue('some string');
+        expect(this.component.props.value).to.equal('');
+      });
+    });
+
+    context('#increment()', function(){
+      it('should increment when value is unset', function(){
+        this.component.setValue('');
+        this.component.increment();
+        expect(this.component.props.value).to.equal(1);
+      });
+
+      it('should increment value', function(){
+        this.component.setValue(100);
+        this.component.increment();
+        this.component.increment();
+        expect(this.component.props.value).to.equal(102);
+      });
+    });
+
+    context('#decrement()', function(){
+      it('should decrement when value is unset', function(){
+        this.component.setValue('');
+        this.component.decrement();
+        expect(this.component.props.value).to.equal(-1);
+      });
+
+      it('should decrement value', function(){
+        this.component.setValue(100);
+        this.component.decrement();
+        this.component.decrement();
+        expect(this.component.props.value).to.equal(98);
+      });
+    });
   });
 
   describe('Behaviors', function(){
