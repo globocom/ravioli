aa10f6ace60fd17c05bf54121d824feb723d5fe0
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-09 13:42:14 -0300

    Adds test utilities

--- test/utils.js
+++ test/utils.js
@@ -5,11 +5,22 @@ global.document = jsdom();
 global.window = document.parentWindow;
 
 module.exports = {
+  findByClass: React.addons.TestUtils.findRenderedDOMComponentWithClass,
+  click: React.addons.TestUtils.Simulate.click,
+
+  keydown: function(element, code){
+    return React.addons.TestUtils.Simulate.keyDown(element, {which: code});
+  },
+
+  input: function(element, value){
+    return React.addons.TestUtils.Simulate.input(element, {
+      target: {value: value}
+    });
+  },
+
   render: function(element, props, child){
     return React.addons.TestUtils.renderIntoDocument(
       React.createElement(element, props, child)
     );
-  },
-
-  findByClass: React.addons.TestUtils.findRenderedDOMComponentWithClass
+  }
 };
