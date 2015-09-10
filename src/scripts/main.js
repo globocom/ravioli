e0c2c271f1ff3ad2c76a78379d351f528959ce24
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-10 19:03:29 -0300

    Create main script to contain all components

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+/* global require, module, window */
+
+var Gnocchi = {
+  Button: require('./components/button'),
+  Number: require('./components/number'),
+  Select: require('./components/select'),
+  Text: require('./components/text'),
+  Textarea: require('./components/textarea')
+};
+
+module.exports = Gnocchi;
+
+if(typeof window !== 'undefined') window.Gnocchi = Gnocchi;
