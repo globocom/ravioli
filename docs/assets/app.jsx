f785562c14ccaa62143a8182a14556a3d7af563b
Almir Filho <almirflorenciofilho@gmail.com>
2015-09-20 16:58:02 -0300

    Adds test app into styleguide page

--- docs/assets/app.jsx
+++ docs/assets/app.jsx
@@ -1,47 +1,47 @@
-var GnocchiText = Gnocchi.Text;
-var GnocchiTextarea = Gnocchi.Textarea;
-var GnocchiNumber = Gnocchi.Number;
-var GnocchiSelect = Gnocchi.Select;
-var GnocchiCheck = Gnocchi.Check;
-var GnocchiButton = Gnocchi.Button;
+var Styleguide = React.createClass({
+  render: function(){
+    return (
+      <main>
+        <section>
+          <h3>Text input</h3>
+          <Gnocchi.Text/>
+        </section>
 
-React.render(
-  <GnocchiText />,
-  document.querySelectorAll('#section-text')[0]
-);
+        <section>
+          <h3>Textarea</h3>
+          <Gnocchi.Textarea/>
+        </section>
 
-React.render(
-  <GnocchiTextarea />,
-  document.querySelectorAll('#section-textarea')[0]
-);
+        <section>
+          <h3>Number input</h3>
+          <Gnocchi.Number/>
+        </section>
 
-React.render(
-  <GnocchiNumber />,
-  document.querySelectorAll('#section-number')[0]
-);
+        <section>
+          <h3>Select</h3>
+          <Gnocchi.Select options={[
+            {value: 'sss', label: 'dasdsadsadasdsa'},
+            {value: 'sswws', label: 'daswwwwdsadsadasdsa'},
+            'sffffffff']}/>
+        </section>
 
-React.render(
-  <GnocchiSelect options={[
-    {value: 'sss', label: 'dasdsadsadasdsa'},
-    {value: 'sswws', label: 'daswwwwdsadsadasdsa'},
-    'sffffffff']}
-  />,
-  document.querySelectorAll('#section-select')[0]
-);
+        <section>
+          <h3>Checkbox</h3>
+          <Gnocchi.Check label='Check me'/>
+        </section>
 
-React.render(
-  <GnocchiCheck />,
-  document.querySelectorAll('#section-check')[0]
-);
+        <section>
+          <h3>Button</h3>
+          <Gnocchi.Button label='Default'/>
+          <Gnocchi.Button label='With icon' icon='arrow-right'/>
+          <Gnocchi.Button label='Action' classes='action'/>
+          <Gnocchi.Button label='Highlight' classes='highlight'/>
+          <Gnocchi.Button label='Lowlight' classes='lowlight'/>
+          <Gnocchi.Button label='Fit' classes='fit'/>
+        </section>
+      </main>
+    );
+  }
+});
 
-React.render(
-  <div>
-    <GnocchiButton label='Default'></GnocchiButton>
-    <GnocchiButton label='With icon' icon='arrow-right'></GnocchiButton>
-    <GnocchiButton label='Action' classes='action' />
-    <GnocchiButton label='Highlight' classes='highlight' />
-    <GnocchiButton label='Lowlight' classes='lowlight' />
-    <GnocchiButton label='Fit' classes='fit' />
-  </div>,
-  document.querySelectorAll('#section-button')[0]
-);
+React.render(<Styleguide/>, document.getElementById('main'));
