ff46817306e02ca640248880e6a223b3732ed68a
Almir Filho <almirflorenciofilho@gmail.com>
2015-10-27 15:28:42 -0200

    Updates react test utils usage

--- test/utils.js
+++ test/utils.js
@@ -11,27 +11,28 @@ var mockDOM = () => {
 
 mockDOM();
 
-var React = require('react/addons');
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
 
 module.exports = {
   mockDOM: mockDOM,
-  findByClass: React.addons.TestUtils.findRenderedDOMComponentWithClass,
-  findAllByClass: React.addons.TestUtils.scryRenderedDOMComponentsWithClass,
-  filterAll: React.addons.TestUtils.findAllInRenderedTree,
-  click: React.addons.TestUtils.Simulate.click,
+  findByClass: TestUtils.findRenderedDOMComponentWithClass,
+  findAllByClass: TestUtils.scryRenderedDOMComponentsWithClass,
+  filterAll: TestUtils.findAllInRenderedTree,
+  click: TestUtils.Simulate.click,
 
   keydown: function(element, code){
-    return React.addons.TestUtils.Simulate.keyDown(element, {which: code});
+    return TestUtils.Simulate.keyDown(element, {which: code});
   },
 
   input: function(element, value){
-    return React.addons.TestUtils.Simulate.input(element, {
+    return TestUtils.Simulate.input(element, {
       target: {value: value}
     });
   },
 
   render: function(element, props, child){
-    return React.addons.TestUtils.renderIntoDocument(
+    return TestUtils.renderIntoDocument(
       React.createElement(element, props, child)
     );
   }
