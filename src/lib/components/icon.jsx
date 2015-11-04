aece5aa12e8677bbc251d94876eb3c4e306db67c
Almir Filho <almirflorenciofilho@gmail.com>
2015-11-04 17:43:08 -0200

    Adds additiona css classes support to icon component

--- src/lib/components/icon.jsx
+++ src/lib/components/icon.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 import classlist from '../helpers/classlist';
 import propsfilter from '../helpers/propsfilter';
 
@@ -6,12 +7,16 @@ import propsfilter from '../helpers/propsfilter';
 export default class GnocchiIcon extends React.Component {
   render(){
     const otherAttrs = propsfilter(this.props, GnocchiIcon.propTypes);
-    const className = classlist('gnocchi-icon', this.props.type);
+    const className = classnames(
+      this.props.className,
+      classlist('gnocchi-icon', this.props.type)
+    );
 
     return <i {...otherAttrs} className={className}></i>;
   }
 }
 
 GnocchiIcon.propTypes = {
-  type: React.PropTypes.string.isRequired
+  type: React.PropTypes.string.isRequired,
+  className: React.PropTypes.string
 };
--- test/components/icon-test.js
+++ test/components/icon-test.js
@@ -6,17 +6,22 @@ var GnocchiIcon = require('../../src/lib/components/icon.jsx');
 describe('Icon component', () => {
   let component;
 
+  const createIcon = opts => component = utils.render(GnocchiIcon, opts);
+  const destroyIcon = () => component = null;
+
   before(() => {
     utils.mockDOM();
-    component = utils.render(GnocchiIcon, {
+    createIcon({
       type: 'bob-dylan',
       rel: 'something',
       onClick: event => event.done()
     });
   });
 
+  after(() => destroyIcon());
+
   it('should render icon', () => {
-    let classes = utils.getDOMNode(component).getAttribute('class');
+    let classes = utils.getDOMNode(component).className;
     expect(classes).to.equal('gnocchi-icon gnocchi-icon-bob-dylan');
   });
 
@@ -32,5 +37,19 @@ describe('Icon component', () => {
 
   it('should set additional prop handlers', done => {
     utils.click(utils.getDOMNode(component), { done: done });
+  });
+
+  describe('with className prop', () => {
+    before(() => createIcon({
+      type: 'dylan',
+      className: 'like-a-rolling-stone'
+    }));
+
+    after(() => destroyIcon());
+
+    it('should set additional css class', () => {
+      let node = utils.getDOMNode(component);
+      expect(node.classList.contains('like-a-rolling-stone')).to.be.true;
+    });
   });
 });
