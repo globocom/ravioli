5508de87e47ed4b3dca94f337481e4336e830cc8
Almir Filho <almirflorenciofilho@gmail.com>
2015-11-09 13:53:03 -0200

    Build

--- dist/lib/components/number.js
+++ dist/lib/components/number.js
@@ -8,7 +8,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
@@ -56,9 +56,10 @@ var GnocchiNumber = (function (_React$Component) {
     key: 'handleTyping',
     value: function handleTyping(event) {
       var key = event.which;
-      var float = this.props.float;
 
-      if ((key < _helpersKeycodes2['default'].N0 || key > _helpersKeycodes2['default'].N9) && (float && key !== _helpersKeycodes2['default'].DOT)) {
+      if (this.props.float) {
+        if (key !== _helpersKeycodes2['default'].DOT && (key < _helpersKeycodes2['default'].N0 || key > _helpersKeycodes2['default'].N9)) event.preventDefault();else if (key === _helpersKeycodes2['default'].DOT && event.target.value.indexOf('.') !== -1) event.preventDefault();
+      } else if (key < _helpersKeycodes2['default'].N0 || key > _helpersKeycodes2['default'].N9) {
         event.preventDefault();
       }
     }
@@ -99,7 +100,7 @@ var GnocchiNumber = (function (_React$Component) {
       var value = _parse2.value;
       var display = _parse2.display;
 
-      if (this.validate(value)) {
+      if (this.validate(value, this.props.float)) {
         if (this.props.onChange && value !== this.state.value) this.props.onChange.call(null, value);
 
         this.setState({ value: value, display: display });
@@ -108,7 +109,12 @@ var GnocchiNumber = (function (_React$Component) {
   }, {
     key: 'validate',
     value: function validate(value) {
-      return !(value < this.props.min || value > this.props.max);
+      var float = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+
+      if (value === '') return true;
+      var isValid = !isNaN(value);
+      if (!float) isValid = isValid && value.toString().indexOf('.') === -1;
+      return isValid && !(value < this.props.min || value > this.props.max);
     }
   }, {
     key: 'parse',
